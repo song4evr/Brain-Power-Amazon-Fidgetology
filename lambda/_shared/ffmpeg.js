0fbea2fb929ce54e07d62e5a672a92fc33996312
Song Jin <sonjin@amazon.com>
2020-09-14 10:10:08 +0000

    fix ffmpeg.js

--- lambda/_shared/ffmpeg.js
+++ lambda/_shared/ffmpeg.js
@@ -1,5 +1,5 @@
 const child_process = require('./child-process-promise');
-const ffmpegPath = process.env.local ? require("@ffmpeg-installer/ffmpeg").path : ("./@ffmpeg-installer/ffmpeg").path;
+const ffmpegPath = process.env.local ? require("@ffmpeg-installer/ffmpeg").path : require("./@ffmpeg-installer/ffmpeg").path;
 
 module.exports = function ffmpeg(commandStr) {
   return child_process.exec(`${ffmpegPath} ${commandStr}`);
